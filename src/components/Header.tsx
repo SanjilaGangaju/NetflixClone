b0dc66063e5e70ed54572f7d9873bb9865df8b66
Sanjila Gangaju <canzilagangaju@gmail.com>
2025-10-24 22:18:51 +0545

    feat(ui): change ui of hero section

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,10 +3,10 @@
 const Header = () => {
   return (
     <header className="flex justify-between items-center px-20 py-10">
-      <div className="text-5xl  text-red-600 font-bold">
+      <div className="text-7xl  text-red-600 font-bold">
          Netflix
       </div>
-      <button className="bg-red-600 rounded px-3 text-center text-m h-8  text-white font-bold">Sign in</button>
+      <button className="bg-red-600 rounded px-4 text-center text-xl h-10  text-white font-bold">Sign in</button>
     </header>  )
 }
 
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,15 +2,15 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div className='flex flex-col gap-10 '>
-         <div className='flex  flex-col items-center'> 
-        <h1 className='text-white font-bold text-center text-4xl'>Unlimited movies, TV shows, and more</h1>
-        <p className='text-white text-xl'>Starts at USD 2.99. Cancel anytime.</p>
+    <div className='flex flex-col gap-10 py-10 h-[80vh] '>
+         <div className='flex  flex-col items-center gap-7'> 
+        <h1 className='text-white w-1/2 font-bold text-center text-6xl'>Unlimited movies, TV shows, and more</h1>
+        <p className='text-white text-2xl font-bold'>Starts at USD 2.99. Cancel anytime.</p>
     </div>
     <div className='flex flex-col items-center gap-4'>
-        <p className='text-white text-xl'>Ready to watch? Enter your email to create or restart your membership.</p>
-        <div className='flex flex-col gap-4 items-center'>
-            <input type="email" placeholder='Email address' className='border rounded text-white p-2 w-70 placeholder:text-white bg-black opacity-50'></input>
+        <p className='text-white text-xl font-semibold w-1/2 text-center'>Ready to watch? Enter your email to create or restart your membership.</p>
+        <div className='flex md:flex-row flex-col gap-4 items-center'>
+            <input type="email" placeholder='Email address' className='border rounded text-white py-3 px-4 w-70 placeholder:text-white bg-black opacity-50'></input>
             <button className='bg-red-600 font-bold text-center text-white p-3 rounded text-xl'>
                 <span>Get started </span>
                 <span>+</span>
